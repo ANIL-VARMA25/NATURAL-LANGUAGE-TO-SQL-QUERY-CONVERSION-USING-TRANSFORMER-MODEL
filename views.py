56be438ae7562438c51d1f574a4e1188d49c593d
ANIL-VARMA25 <anilvarmakonduru@gmail.com>
2025-07-25 19:21:55 +0530

    Add files via upload

--- views.py
+++ views.py
@@ -1,44 +1,19 @@
-from django.shortcuts import render,HttpResponse
-from django.contrib import messages
-from users.models import UserRegistrationModel,QueryLog
+from django.shortcuts import render
+import requests
+from users.forms import UserRegistrationForm
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html', {})
 
-def AdminLoginCheck(request):
-    if request.method == 'POST':
-        usrid = request.POST.get('loginid')
-        pswd = request.POST.get('pswd')
-        print("User ID is = ", usrid)
-        if usrid == 'admin' and pswd == 'admin':
-            return render(request, 'admins/AdminHome.html')
-        elif usrid == 'Admin' and pswd == 'Admin':
-            return render(request, 'admins/AdminHome.html')
-        else:
-            messages.success(request, 'Please Check Your Login Details')
+def logout(request):
+    return render(request, 'index.html', {})
+
+def UserLogin(request):
+    return render(request, 'UserLogin.html', {})
+
+def AdminLogin(request):
     return render(request, 'AdminLogin.html', {})
 
-
-def AdminHome(request):
-    return render(request, 'admins/AdminHome.html')
-
-
-def ViewRegisteredUsers(request):
-    data = UserRegistrationModel.objects.all()
-    return render(request, 'admins/RegisteredUsers.html', {'data': data})
-
-
-def AdminActivaUsers(request):
-    if request.method == 'GET':
-        id = request.GET.get('uid')
-        status = 'activated'
-        print("PID = ", id, status)
-        UserRegistrationModel.objects.filter(id=id).update(status=status)
-        data = UserRegistrationModel.objects.all()
-        return render(request, 'admins/RegisteredUsers.html', {'data': data})
-
-def AllUserResults(request):
-    data = QueryLog.objects.all()
-    return render(request, "admins/AdminAllResults.html", {"data": data})
-
-def adminCNNModel(request):
-    return HttpResponse("CNN work fine")
+def UserRegister(request):
+    form = UserRegistrationForm()
+    return render(request, 'UserRegistrations.html', {'form': form})
